4b3be4d24671c0c7d9ea43c9c91a2a793562eb50
Gabalbur <alternativegabrielmarques619@gmail.com>
2022-06-17 08:54:24 -0400

    MasterFirstCommit

---
+++ app/src/main/java/com/example/irrsystem/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.irrsystem
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.irrsystem.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private var _binding: ActivityMainBinding? = null
+    private val binding: ActivityMainBinding? = _binding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
+    }
+}
